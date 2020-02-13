2d05808caf5ee17b8b81fb1e4bbb678511f71877
pussstudio <pusan.speaker@gmail.com>
2020-02-14 01:16:38 +0900

    init

--- client/src/App.js
+++ client/src/App.js
@@ -19,43 +19,31 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
+});
 
 
 
-
-
-const customers = [
-  
-  {
-  'id': 1,
-  'image': 'http://placeimg.com/64/64/1',
-  'name' : '홍길동',
-  'birthday' : '961222',
-  'gender' : '남자',
-  'job': '대학생'
-},
-{
-  'id': 2,
-  'image': 'http://placeimg.com/64/64/2',
-  'name' : '단무지',
-  'birthday' : '961222',
-  'gender' : '여자',
-  'job': '언론사'
-},
-{
-  'id': 3,
-  'image': 'http://placeimg.com/64/64/any',
-  'name' : '이순신',
-  'birthday' : '961222',
-  'gender' : '남자',
-  'job': '디자이너'
-}
-
-]
 
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+  
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+  
+  callApi = async () => {
+    const response = await fetch('api/customers')
+    const body = await response.json();
+    return body;
+  }
+
+
   render() {
     const {classes} = this.props;
     return (
@@ -71,14 +59,16 @@ class App extends Component {
           </TableHead>
         <TableBody>
         {
-        customers.map(c => { return (
-            <Customer
-            key={c.id} //map을 사용할때는 key라는 것을 추가로 넣어야 된다. 각 오브젝트마다 다른 고유의 값을 넣어야된다.
-            id = {c.id} image = {c.image}  name={c.name} 
-            birthday={c.birthday} gender={c.gender}  job={c.job}
-            />
-          )
-        })
+          this.state.customers ?
+              this.state.customers.map(c => { return (
+                  <Customer
+                  key={c.id} //map을 사용할때는 key라는 것을 추가로 넣어야 된다. 각 오브젝트마다 다른 고유의 값을 넣어야된다.
+                  id = {c.id} image = {c.image}  name={c.name} 
+                  birthday={c.birthday} gender={c.gender}  job={c.job}
+                  />
+                )
+              })
+              :  ""
         }
         </TableBody>
 
--- server.js
+++ server.js
@@ -7,10 +7,37 @@ server.use(bodyParser.json());
 // server.use('api', require('./routes/api'));
 server.use(bodyParser.urlencoded({extended:true}));
 
-
-server.get('/api/hello', (req, res)=>{
-    res.send({message: 'Hello Express!'});
+server.get('/api/customers', (req, res)=>{
+    res.send([
+  
+        {
+        'id': 1,
+        'image': 'http://placeimg.com/64/64/1',
+        'name' : '홍길동',
+        'birthday' : '961222',
+        'gender' : '남자',
+        'job': '대학생'
+      },
+      {
+        'id': 2,
+        'image': 'http://placeimg.com/64/64/2',
+        'name' : '단무지',
+        'birthday' : '961222',
+        'gender' : '여자',
+        'job': '언론사'
+      },
+      {
+        'id': 3,
+        'image': 'http://placeimg.com/64/64/any',
+        'name' : '이순신',
+        'birthday' : '961222',
+        'gender' : '남자',
+        'job': '디자이너'
+      }
+      
+      ]);
 });
+
 
 server.listen(5000, (err) => {   
     console.log(`The server is running --- ${port}`);
