1e1d7e8445065033d5582776852b647534461c6a
pussstudio <pusan.speaker@gmail.com>
2020-02-15 23:20:25 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs') //파일에 접근할 수 있는 라이브러리
 const express = require('express');
 const bodyParser = require('body-parser');
 const server = express();
@@ -7,36 +8,28 @@ server.use(bodyParser.json());
 // server.use('api', require('./routes/api'));
 server.use(bodyParser.urlencoded({extended:true}));
 
-server.get('/api/customers', (req, res)=>{
-    res.send([
-  
-        {
-        'id': 1,
-        'image': 'http://placeimg.com/64/64/1',
-        'name' : '홍길동',
-        'birthday' : '961222',
-        'gender' : '남자',
-        'job': '대학생'
-      },
-      {
-        'id': 2,
-        'image': 'http://placeimg.com/64/64/2',
-        'name' : '단무지',
-        'birthday' : '961222',
-        'gender' : '여자',
-        'job': '언론사'
-      },
-      {
-        'id': 3,
-        'image': 'http://placeimg.com/64/64/any',
-        'name' : '이순신',
-        'birthday' : '961222',
-        'gender' : '남자',
-        'job': '디자이너'
-      }
-      
-      ]);
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
 });
+connection.connect();
+
+server.get('/api/customers', (req, res)=>{
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
+});
+
 
 
 server.listen(5000, (err) => {   
